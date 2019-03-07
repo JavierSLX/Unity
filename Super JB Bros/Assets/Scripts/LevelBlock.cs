84a2160f59dbe8ee343b7ce32902cb976add3c65
JavierSLX <jserranolule@gmail.com>
2019-03-06 20:07:15 -0600

    Prefabs level blocks

---
+++ Super JB Bros/Assets/Scripts/LevelBlock.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelBlock : MonoBehaviour
+{
+    public Transform startPoint;    //Los puntos de donde inicia y termina el nivel
+    public Transform exitPoint;
+
+	
+}
