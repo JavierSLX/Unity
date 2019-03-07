7244311338e5cf058f02e1d2bdde7b8ad9bf852e
JavierSLX <jserranolule@gmail.com>
2019-03-06 20:37:20 -0600

    Iniciando generacion de niveles aleatorios

--- Super JB Bros/Assets/Scripts/LevelGenerator.cs
+++ Super JB Bros/Assets/Scripts/LevelGenerator.cs
@@ -2,15 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelGenerator : MonoBehaviour {
+//Script Singleton para generar trozos de nivel
+public class LevelGenerator : MonoBehaviour
+{
+    public static LevelGenerator getInstance;
+    public List<LevelBlock> allTheLevelBlocks = new List<LevelBlock>(); //Una lista de todos los bloques de nivel
+    public Transform levelStartPoint; //Donde se encuentra el punto de inicio
+    public List<LevelBlock> currentBlocks = new List<LevelBlock>(); //Los bloques que estan en la escena
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private void Awake()
+    {
+        getInstance = this;
+    }
+
+    private void Start()
+    {
+        AddLevelBlock();
+    }
+
+    //Agrega un nuevo bloque de nivel
+    public void AddLevelBlock()
+    {
+        //Saca un numero aleatorio
+        int ramdomIndex = Random.Range(0, allTheLevelBlocks.Count);
+
+        //Instanciamos el bloque actual de la lista
+        LevelBlock currentBlock = (LevelBlock)Instantiate(allTheLevelBlocks[ramdomIndex]);
+
+        //Lo añade como hijo e la jerarquia
+        currentBlock.transform.SetParent(this.transform, false);
+
+        //Para generar el escenario desde el centro
+        Vector3 spawnPosition = Vector3.zero;
+
+        //Si es el primero
+        if(currentBlocks.Count == 0)
+        {
+            spawnPosition = levelStartPoint.position;
+        }
+        else
+        {
+            spawnPosition = currentBlocks[currentBlocks.Count - 1].exitPoint.position;
+        }
+
+        //Agrega la posicion y a la lista
+        currentBlock.transform.position = spawnPosition;
+        currentBlocks.Add(currentBlock);
+    }
+
+    //Elimina el bloque más viejo
+    public void RemoveOldestLevelBlock()
+    {
+
+    }
+
+    //Quita todos los bloques del nivel
+    public void RemoveAllTheBlocks()
+    {
+
+    }
+
+    //Genera los bloques iniciales del juego
+    public void GenerateInitialBlocks()
+    {
+
+    }
 }
