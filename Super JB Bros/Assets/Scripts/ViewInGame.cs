a93e7e917bf8096264f504c937bbd75d92241870
JavierSLX <jserranolule@gmail.com>
2019-03-13 15:54:13 -0600

    Contador en pantalla

---
+++ Super JB Bros/Assets/Scripts/ViewInGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ViewInGame : MonoBehaviour
+{
+    //Las etiquetas de la pantalla
+    public Text collectableLabel;
+    public Text scoreLabel;
+    public Text maxScoreLabel;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        //Si el estado del juego esta IN GAME se actualiza el contador de monedas
+		if(GameManager.getInstance.currentGameState == GameState.IN_GAME)
+        {
+            int currentObjects = GameManager.getInstance.collectedObject;
+            this.collectableLabel.text = currentObjects.ToString();
+        }
+	}
+}
