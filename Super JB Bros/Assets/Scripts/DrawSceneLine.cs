b2dfe96292acc4bd55193ec24a7bafdd223377b8
JavierSLX <jserranolule@gmail.com>
2019-03-20 16:03:27 -0600

    Plataforma moviles!

---
+++ Super JB Bros/Assets/Scripts/DrawSceneLine.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawSceneLine : MonoBehaviour
+{
+    public Transform origin;
+    public Transform to;
+
+    private void OnDrawGizmosSelected()
+    {
+        //Dibuja la linea
+        if(origin != null && to != null)
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawLine(origin.position, to.position);
+            Gizmos.DrawSphere(origin.position, 0.15f);
+            Gizmos.DrawSphere(to.position, 0.15f);
+        }
+    }
+}
---
+++ Super JB Bros/Assets/Scripts/PlatformMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMove : MonoBehaviour
+{
+    public Transform target;
+    public Transform origin;
+    public float speed;
+    private Vector3 start, end;
+
+    private void Start()
+    {
+        if(origin != null && target != null)
+        {
+            start = origin.position;
+            end = target.position;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(origin != null && target != null)
+        {
+            float fixedSpeed = speed * Time.deltaTime;
+            origin.position = Vector3.MoveTowards(origin.position, target.position, fixedSpeed);
+        }
+
+        //Se cambian las posiciones cuando estas se igualan
+        if(origin.position == target.position)
+        {
+            target.position = target.position == start ? end : start;
+        }
+    }
+}
